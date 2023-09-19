ecfcf9416cf4e07aa783a6ba9acbc9ec62faa9b6
Ivan Dimitrov <ivan@idimitrov.dev>
2023-09-19 15:22:42 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    simple-nixos-mailserver = {
+      url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    "idimitrov-dev" = {
+      url = "git+ssh://git@gitlab.com:ivandimitrov8080/idimitrov.dev.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , simple-nixos-mailserver
+    , idimitrov-dev
+    , ...
+    }: {
+      nixosConfigurations = {
+        mailserver = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            simple-nixos-mailserver.nixosModule
+            idimitrov-dev
+            ./mailserver
+          ];
+        };
+      };
+    };
+}
