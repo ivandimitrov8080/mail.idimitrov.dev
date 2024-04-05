b163c249fa3a56a083c7892162bdf12c83f2d66f
Ivan Dimitrov <ivan@idimitrov.dev>
2024-04-05 17:54:34 +0300

    restrict roundcube only to vpn

--- mailserver/nginx/default.nix
+++ mailserver/nginx/default.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
   webshiteConfig = ''
     add_header 'Referrer-Policy' 'origin-when-cross-origin';
     add_header X-Content-Type-Options nosniff;
     add_header Onion-Location http://sxfx23zafag4lixkb4s6zwih7ga5jnzfgtgykcerd354bvb6u7alnkid.onion;
+  '';
+  restrictToVpn = ''
+    allow 10.0.0.2/32;
+    allow fdc9:281f:04d7:9ee9::2/128;
+    allow 10.0.0.3/32;
+    deny all;
   '';
 in
 {
@@ -31,6 +37,9 @@ in
             proxyPass = "http://127.0.0.1:3000";
           };
           extraConfig = webshiteConfig;
+        };
+        "${config.mailserver.fqdn}" = {
+          extraConfig = restrictToVpn;
         };
         "src.idimitrov.dev" = {
           enableACME = true;
